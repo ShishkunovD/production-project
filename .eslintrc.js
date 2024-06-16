ea69e210312c89f151e54fab8f73bbe38380a3b2
Dmitriy Shishkunov <dshishkunov@gmcs.ru>
2024-06-16 12:43:32 +0300

    add stylelint, i18n plugin for eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,100 +1,45 @@
 module.exports = {
-    globals: {
-      JSX: true,
-      '__IS_DEV__': true,
-    },
-    parserOptions: {
-      project: './tsconfig.json',
+  env: {
+      browser: true,
+      es2021: true,
+  },
+  extends: [
+      'plugin:react/recommended',
+      'airbnb',
+      'plugin:i18next/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
       ecmaFeatures: {
-        jsx: true,
+          jsx: true,
       },
-      ecmaVersion: 2018,
+      ecmaVersion: 'latest',
       sourceType: 'module',
-    },
-    settings: {
-      'import/resolver': {
-        typescript: {},
-        node: {
-          extensions: ['.js', '.jsx', '.ts', '.tsx'],
-        },
-      },
-    },
-    extends: ['airbnb-typescript-prettier', 'eslint:recommended'],
-    env: {
-      jest: true,
-    },
-    rules: {
-      'no-param-reassign': ['error', { props: false }],
+  },
+  plugins: [
+      'react',
+      '@typescript-eslint',
+      'i18next',
+  ],
+  rules: {
+      'react/jsx-indent': [2, 4],
+      'react/jsx-indent-props': [2, 4],
+      indent: [2, 4],
+      'react/jsx-filename-extension': [2, { extensions: ['.js', '.jsx', '.tsx'] }],
+      'import/no-unresolved': 'off',
       'import/prefer-default-export': 'off',
-      'react/require-default-props': 'off',
-      '@typescript-eslint/explicit-module-boundary-types': 'off',
-      'react/jsx-props-no-spreading': 'warn',
-      'import/no-extraneous-dependencies': 'off',
-      'import/no-named-as-default': 'off',
-      'react/react-in-jsx-scope': 'off',
-      'no-underscore-dangle': 'off',
-      'react/function-component-definition': [
-        2,
-        {
-          namedComponents: 'arrow-function',
-          unnamedComponents: 'arrow-function',
-        },
-      ],
-      'jsx-a11y/anchor-is-valid': [
-        'error',
-        {
-          components: ['Link'],
-          specialLink: ['hrefLeft', 'hrefRight'],
-          aspects: ['invalidHref', 'preferButton'],
-        },
-      ],
-      '@typescript-eslint/no-unused-vars': ['warn', { varsIgnorePattern: '^_' }],
       'no-unused-vars': 'warn',
-      '@typescript-eslint/ban-ts-comment': 'off',
-      'jsx-a11y/label-has-associated-control': [
-        'error',
-        {
-          required: {
-            some: ['nesting', 'id'],
-          },
-        },
-      ],
-      'jsx-a11y/label-has-for': [
-        'error',
-        {
-          required: {
-            some: ['nesting', 'id'],
-          },
-        },
-      ],
-      'jsx-a11y/click-events-have-key-events': 'off',
-      'jsx-a11y/interactive-supports-focus': 'off',
-      'import/no-unresolved': [2, { caseSensitive: false }],
-      'import/order': [
-        'error',
-        {
-          pathGroups: [
-            {
-              pattern: 'react',
-              group: 'builtin',
-              position: 'before',
-            },
-            {
-              pattern: 'src/**',
-              group: 'internal',
-              position: 'after',
-            },
-          ],
-          pathGroupsExcludedImportTypes: ['react', 'src/**'],
-          groups: [
-            ['builtin', 'external', 'internal'],
-            ['parent', 'sibling'],
-          ],
-          'newlines-between': 'always-and-inside-groups',
-        },
-      ],
-      'class-methods-use-this': ['error', { enforceForClassFields: false }],
-    },
-  
-  };
-  
+      'react/require-default-props': 'off',
+      'react/react-in-jsx-scope': 'off',
+      'react/jsx-props-no-spreading': 'warn',
+      'react/function-component-definition': 'off',
+      'no-shadow': 'off',
+      'import/extensions': 'off',
+      'import/no-extraneous-dependencies': 'off',
+      'no-underscore-dangle': 'off',
+      'i18next/no-literal-string': ['error', { markupOnly: true }],
+  },
+  globals: {
+      __IS_DEV__: true,
+  },
+};
--- src/shared/ui/LangSwitcher/LangSwitcher.tsx
+++ src/shared/ui/LangSwitcher/LangSwitcher.tsx
@@ -2,7 +2,6 @@ import { classNames } from 'shared/lib/classNames/classNames';
 
 import { useTranslation } from 'react-i18next';
 
-import cls from './LangSwitcher.module.scss';
 import { Button, ThemeButton } from '../Button/ui/Button';
 
 interface LangSwitcherProps {
@@ -10,21 +9,21 @@ interface LangSwitcherProps {
 }
 
 const LangSwitcher = ({ className }: LangSwitcherProps) => {
-  const { t, i18n } = useTranslation();
+    const { t, i18n } = useTranslation();
 
-  const toggle = () => {
-    i18n.changeLanguage(i18n.language === 'ru' ? 'en' : 'ru');
-  };
+    const toggle = () => {
+        i18n.changeLanguage(i18n.language === 'ru' ? 'en' : 'ru');
+    };
 
-  return (
-    <Button
-      className={classNames(cls.LangSwitcher, {}, [className])}
-      theme={ThemeButton.CLEAR}
-      onClick={toggle}
-    >
-      {t('Язык')}
-    </Button>
-  );
+    return (
+        <Button
+            className={classNames('', {}, [className])}
+            theme={ThemeButton.CLEAR}
+            onClick={toggle}
+        >
+            {t('Язык')}
+        </Button>
+    );
 };
 
 export { LangSwitcher };
